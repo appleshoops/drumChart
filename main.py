43bc991ff027b20428e6f39739ec62ab015a1b00
appleshoops <95598768+appleshoops@users.noreply.github.com>
2025-09-17 08:21:18 +1000

    oijegraijoperagjiopfsv

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from tkinter import ttk
 from tkinter import filedialog as fd
 import mido
 
-def convert_midi(file_path):
-    midi_file = mido.MidiFile(file_path)
-
 
 root = tk.Tk()
 
@@ -19,6 +16,10 @@ path_var = tk.StringVar(value="No file selected")
 top_bar = ttk.Frame(root, padding=(10, 10))
 top_bar.grid(row=0, column=0, sticky="ew")
 top_bar.columnconfigure(1, weight=1)
+
+bottom_bar = ttk.Frame(root, padding=(10, 10))
+bottom_bar.grid(row=1, column = 1, sticky="sw")
+bottom_bar.columnconfigure(1, weight=1)
 
 def select_midi():
     filetypes = (
@@ -34,6 +35,9 @@ def select_midi():
     if filename:
         path_var.set(filename)
 
+def convert_midi(file_path):
+    midi_file = mido.MidiFile(file_path, clip=True)
+
 open_button = ttk.Button(
     top_bar,
     text="Select MIDI File",
@@ -43,5 +47,12 @@ open_button.grid(row=0, column=0, padx=(0, 10), sticky="w")
 
 path_entry = ttk.Entry(top_bar, textvariable=path_var, state="readonly")
 path_entry.grid(row=0, column=1, sticky="ew")
+
+start_button = ttk.Button(
+    bottom_bar,
+    text="Start!!!",
+    command=lambda: convert_midi(path_var.get())
+)
+start_button.grid(row=1, column=1, sticky="sw")
 
 root.mainloop()

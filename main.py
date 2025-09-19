c346e4dd06411796d4bdc8fe188529384a8fc25e
appleshoops <95598768+appleshoops@users.noreply.github.com>
2025-09-19 14:44:26 +1000

    bro knowing

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog as fd
 from midi_convert import convert_midi
-import mido
 
 
 root = tk.Tk()
--- midi_convert.py
+++ midi_convert.py
@@ -1,6 +1,7 @@
 import mido
-import tkinter
 from tkinter.messagebox import showinfo
+from tkinter import filedialog as fd
+import os
 
 def convert_midi(file_path):
     if file_path == "No file selected":
@@ -8,5 +9,30 @@ def convert_midi(file_path):
         return
     else:
         mid = mido.MidiFile(file_path, clip=True)
+        newMid = mido.MidiFile()
+        newTrack = mido.MidiTrack()
         for track in mid.tracks:
-            print(track)
+            for msg in track:
+                if msg.type in 'set_tempo':
+                    newTrack.append(msg)
+                    # bpm = mido.tempo2bpm(msg.tempo)
+                if msg.type in ['note_on', 'note_off'] and msg.channel == 9:
+                    newTrack.append(msg)
+        newMid.ticks_per_beat = mid.ticks_per_beat
+        newMid.tracks.append(newTrack)
+
+        save_path = os.path.expanduser(savefilepath().name)
+        newMid.save(save_path)
+
+def savefilepath():
+    filetypes = (
+        [("MIDI File", "*.mid")]
+    )
+
+    path = fd.asksaveasfile(
+        mode='w',
+        defaultextension='.mid',
+        filetypes=filetypes
+    )
+
+    return path
